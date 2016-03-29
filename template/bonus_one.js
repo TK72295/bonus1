8e6e45073e9f1617b8da51fbef5703ce5e49f63a
Tyler Kim <tak90@pitt.edu>
2016-03-28 21:29:12 -0400

    Initial commit

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -14,4 +14,58 @@
 (function() {
 	// Magic!
 	console.log('Keepin\'n it clean with an external script!');
+
+	//Clear screen
+	$(".flexsearch-input").val("");
+
+	//Pull data from api
+	var dataStuff;
+	var jqxhr = $.ajax({
+		dataType: "json",
+		url: "http://www.mattbowytz.com/simple_api.json?data=all",
+		error: function(){
+			console.log("ERROR");
+		},
+		success: function(data){
+			dataStuff = data['data'];
+			console.log(dataStuff);
+		},
+		type: 'GET'
+	});
+
+	//Compare the search input to "data"
+	var searchInput;
+	$(".flexsearch-input").keyup(function(){
+		searchInput = $(".flexsearch-input").val();
+		var strings=new Array();
+		for(var i = 0; i < dataStuff['programming'].length; i++){
+			if(searchInput.toLowerCase() == dataStuff['programming'][i].substring(0, searchInput.length).toLowerCase()){
+				strings.push(dataStuff['programming'][i]);
+			}
+		}
+		for(var i = 0; i < dataStuff['interests'].length; i++){
+			if(searchInput.toLowerCase() == dataStuff['interests'][i].substring(0, searchInput.length).toLowerCase()){
+				strings.push(dataStuff['interests'][i]);
+			}
+		}
+
+		if(searchInput.length > 0 && strings.length > 0){
+			var htmlOut = "";
+			for(var i = 0; i < strings.length; i++){
+				var link = "https://www.google.com/#q=" + strings[i];
+				htmlOut += "<a href = " + link + ">" + strings[i] + "</a></br>"
+				}
+			$("#reply").html(htmlOut);
+		}
+		else
+			$("#reply").text("");
+	});
+
+	$(function(){
+		$("#mainForm").submit(function(){
+			var url = "https://www.google.com/#q=" + $(".flexsearch-input").val();
+			window.location = url;
+			return false;
+		});
+	});
 })();
